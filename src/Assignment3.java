48d44b11727a8708cf436995bddbfd8f6f31c096
Your Name <you@example.com>
2026-09-02 20:51:15 +0800

    Complete Assignment 3

--- src/Assignment3.java
+++ src/Assignment3.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+public class Assignment3 { // Declares a public class named Assignment3
 
-public class Assignment3 {
-}
+    public static void main(String[] args) { // Defines the main method where the program starts
+        String firstname = "Trisha Belle"; // Stores the user's first name
+        String lastname = "Taruma"; // Stores the user's lastname
+        String address = "Villasis"; // Stores the users address
+        String mobileNumber = "09564612707"; // Stores the user's mobile number
+        int age = 20; // Stores the user's age
+        double gwa = 92.5; // Stores the user's GWA
+
+        System.out.println("Firstname: " + firstname); // Displays the first name;
+        System.out.println("Lastname: " + lastname); // Displays the lastname
+        System.out.println("Address: " + address); // Displays the address
+        System.out.println("Mobile Number: " + mobileNumber); // Displays the mobile number
+        System.out.println("Age: " + age); // Displays the age
+        System.out.println("GWA: " + gwa); //Displays the GWA
+    } // Closes the main method
+} // Closes the Assignment3 class
+
+
+
